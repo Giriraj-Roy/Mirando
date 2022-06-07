38f87a79ec0292855c8725c8a1f47f08f285d437
Giriraj Roy <girirajroy2001@gmail.com>
2022-06-07 22:16:34 +0530

    Display thumnails

--- mirando/src/components/Main.js
+++ mirando/src/components/Main.js
@@ -17,6 +17,15 @@ const Main = () => {
 
     const imageUrl = `https://image.tmdb.org/t/p/original${movie?.backdrop_path}`;
 
+    const truncateString = (str,num) =>{
+        if(str?.length > num){
+            return str.slice(0, num) + '...';
+        }
+        else{
+            return str;
+        }
+    }
+
 
     return (
     <div className='w-full h-[550px] text-white'>
@@ -34,7 +43,9 @@ const Main = () => {
                     </button>
                 </div>
                 <p className='text-gray-400 text-sm'>Released : {movie?.release_date}</p>
-                <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{movie?.overview}</p>
+                <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>
+                    {truncateString(movie?.overview, 150)}
+                </p>
             </div>
 
         </div>
