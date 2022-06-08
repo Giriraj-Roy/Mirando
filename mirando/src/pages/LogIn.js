e4943f15692784779876d01436950d85edb28b82
Giriraj Roy <girirajroy2001@gmail.com>
2022-06-08 18:44:31 +0530

    Updated account page
    Signed-off-by: Giriraj Roy <girirajroy2001@gmail.com>

--- mirando/src/pages/LogIn.js
+++ mirando/src/pages/LogIn.js
@@ -28,7 +28,8 @@ const LogIn = () => {
       <div className='w-full h-screen'>
         <img
         className="hidden  sm:block absolute w-full h-full object-cover"
-        src='https://assets.nflxext.com/ffe/siteui/vlv3/5ea364b1-8e59-4693-8ad8-f0eaee32d1bf/5b9fdff5-be3d-4fc6-9606-dd1b4002f277/IN-en-20220530-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt="home" />
+        src='https://assets.nflxext.com/ffe/siteui/vlv3/5ea364b1-8e59-4693-8ad8-f0eaee32d1bf/5b9fdff5-be3d-4fc6-9606-dd1b4002f277/IN-en-20220530-popsignuptwoweeks-perspective_alpha_website_medium.jpg'
+        alt="home" />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'>
           <div className='w-full fixed px-4 py-24 z-50'>
             <div className='max-w-[450px] h-[600px] mx-auto bg-black text-white'>
